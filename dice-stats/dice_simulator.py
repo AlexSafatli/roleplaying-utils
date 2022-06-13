019cf9777b9fd565c20b2f107a3d223fd5975718
Alex Safatli <alexs@liberateme.com>
2022-06-13 20:52:04 -0300

    Convert dice_simulator to Python 3

--- dice-stats/dice_simulator.py
+++ dice-stats/dice_simulator.py
@@ -9,62 +9,70 @@ import random as r
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
-class die:
-    def __init__(self,num,sides):
+
+class Die:
+
+    def __init__(self, num: int, sides: int):
         self.num = num
         self.sides = sides
+
     def roll(self):
         result = 0
-        for i in xrange(self.num): result += r.randint(1,self.sides)
+        for i in range(self.num):
+            result += r.randint(1, self.sides)
         return result
 
-class diceroller:
-    def __init__(self,inputstr):
+
+class DiceRoller:
+
+    def __init__(self, inputstr):
         self.string = inputstr
         self.dice = []
         self.modifier = 0
         self.__parse__()
+
     def __parse__(self):
-        dic = re.compile('[0-9]*d[0-9]*')
-        posmod = re.compile('\+[0-9]*')
-        negmod = re.compile('\-[0-9]*')
+        dic = re.compile('\d*d\d*')
+        posmod = re.compile('\+\d*')
+        negmod = re.compile('-\d*')
         for d in dic.finditer(self.string):
             d = d.group(0).split('d')
-            self.dice.append(die(int(d[0]),int(d[1])))
-        for mod in posmod.finditer(self.string):
+            self.dice.append(Die(int(d[0]), int(d[1])))
+        for _ in posmod.finditer(self.string):
             d = d.group(0).strip('+')
             self.modifier += int(d)
-        for mod in negmod.finditer(self.string):
+        for _ in negmod.finditer(self.string):
             d = d.group(0).strip('-')
             self.modifier -= int(d)
-    def rollAll(self,num):
+
+    def roll_all(self, num):
         out = []
-        for x in xrange(num):
+        for x in range(num):
             tot = 0
-            for d in self.dice: tot += d.roll()
+            for di in self.dice:
+                tot += di.roll()
             tot += self.modifier
             out.append(tot)
         return out
 
-# Get input.
 
+# Get input.
 inp = sys.argv
-if len(inp) != 4: print 'usage: python diceSimulator.py XdY+Z NUM_SAMPLES OUTPUT_NAME'
+if len(inp) != 4:
+    print('usage: python diceSimulator.py XdY+Z NUM_SAMPLES OUTPUT_NAME')
 roll = inp[1]
-numtimes = int(inp[2])
-print 'Performing %s multiple times (number samples: %d).' % (roll,numtimes)
+num_times = int(inp[2])
+print('Performing %s multiple times (number samples: %d).' % (roll, num_times))
 outname = inp[3]
 
 # Roll (like an 18-wheela').
-
-d = diceroller(roll)
-arr = d.rollAll(numtimes)
+d = DiceRoller(roll)
+arr = d.roll_all(num_times)
 
 # Plot.
-
 plt.hist(arr)
 plt.title(roll)
-plt.ylabel('Number of Occurences per %d Samples' % (numtimes))
+plt.ylabel('Number of Occurences per %d Samples' % (num_times))
 pp = PdfPages(outname)
-plt.savefig(pp,format='pdf')
+plt.savefig(pp, format='pdf')
 pp.close()
